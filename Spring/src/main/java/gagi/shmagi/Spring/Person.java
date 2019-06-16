7aa4fa9b18f2586ebaf6d0291e1155348c0416cf
gagi <gagi.shmagi@gmail.com>
2019-06-17 00:06:30 +0300

    Added Spring Boot Api Person Entity

---
+++ Spring/src/main/java/gagi/shmagi/Spring/Person.java
@@ -0,0 +1,63 @@
+package gagi.shmagi.Spring;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @NotNull
+    private String firstname;
+
+    @NotNull
+    private String lastname;
+
+    public Person() {
+    }
+
+    public Person(@NotNull String firstname, @NotNull String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setgetLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
