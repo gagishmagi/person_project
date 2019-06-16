bd1d4a4878e9cacbbfd2bef8a8eabe4d9150f4ec
gagi <gagi.shmagi@gmail.com>
2019-06-17 00:25:45 +0300

    Modified default Angular component to use http request service
    And Spring Boot API backend

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpRequestService } from './http-request.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+    title = 'Angular';
+    url = 'http://localhost:8080/people';
+
+    public data: any;
+
+    constructor(private api: HttpRequestService) {
+    }
+
+    ngOnInit() {
+        this.api
+        .getPersonen(this.url)
+        .subscribe(
+            data => {
+                console.log(data);
+                this.data = data;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
 }
