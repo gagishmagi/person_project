8f2c13efeb3751cc76426c34a37024f159b0f782
gagi <gagi.shmagi@gmail.com>
2019-06-16 23:56:01 +0300

    Added Spring boot App to project

---
+++ Spring/src/test/java/gagi/shmagi/Spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gagi.shmagi.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
